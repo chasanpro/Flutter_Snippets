279fc0e300a7bf5ff714f7c285a0a00009a6a865
Chaitanya Damarasingu <chasanpro@gmail.com>
2023-04-04 22:34:00 +0530

    Create List_Provider.dart

---
+++ List_Provider.dart
@@ -0,0 +1,65 @@
+In Flutter, you can use a provider to manage a list fetched from an API by following these steps:
+
+First, create a provider class that will hold the state of the list. This class should extend the ChangeNotifier class.
+dart
+Copy code
+import 'package:flutter/material.dart';
+
+class MyListProvider with ChangeNotifier {
+  List<dynamic> _myList = [];
+
+  List<dynamic> get myList => _myList;
+
+  void setMyList(List<dynamic> myList) {
+    _myList = myList;
+    notifyListeners();
+  }
+}
+Next, create a function that will fetch the data from the API and update the state of the provider.
+dart
+Copy code
+import 'package:http/http.dart' as http;
+
+Future<void> fetchData(MyListProvider provider) async {
+  final response = await http.get(Uri.parse('https://api.example.com/mylist'));
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    provider.setMyList(data);
+  } else {
+    throw Exception('Failed to load data from API');
+  }
+}
+Now, you can use the provider in your Flutter widget. In this example, we will use the Consumer widget to rebuild the widget tree whenever the state of the provider changes.
+dart
+Copy code
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MyListProvider>(
+      builder: (context, provider, child) {
+        if (provider.myList.isEmpty) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          return ListView.builder(
+            itemCount: provider.myList.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(provider.myList[index]['title']),
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+}
+Finally, you can call the fetchData function to fetch the data from the API and update the state of the provider.
+dart
+Copy code
+fetchData(context.read<MyListProvider>());
+By following these steps, you can use a provider to manage a list fetched from an API in Flutter, and easily update the state of your app whenever the list changes.
