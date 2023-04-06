3e8958520449adc2958ffa7012b29eda7a0bcced
Chaitanya Damarasingu <chasanpro@gmail.com>
2023-04-06 17:26:42 +0530

    Create Data_table.dart

---
+++ Data_table.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Data Table Example'),
+        ),
+        body: DataTableDemo(),
+      ),
+    );
+  }
+}
+class DataTableDemo extends StatefulWidget {
+  @override
+  _DataTableDemoState createState() => _DataTableDemoState();
+}
+
+class _DataTableDemoState extends State<DataTableDemo> {
+  // A list of sample data to display in the table
+  List<Map<String, dynamic>> data = [
+    {'id': 1, 'name': 'Alice', 'age': 20},
+    {'id': 2, 'name': 'Bob', 'age': 25},
+    {'id': 3, 'name': 'Charlie', 'age': 30},
+    {'id': 4, 'name': 'David', 'age': 35},
+    {'id': 5, 'name': 'Eve', 'age': 40},
+  ];
+  
+  // A function to create a list of DataColumns from the keys of the data map
+  List<DataColumn> _createColumns() {
+    return data[0].keys.map((key) => DataColumn(label: Text(key))).toList();
+  }
+
+  // A function to create a list of DataRows from the values of the data map
+  List<DataRow> _createRows() {
+    return data.map((row) {
+      return DataRow(
+        cells: row.values.map((value) => DataCell(Text(value.toString()))).toList(),
+      );
+    }).toList();
+  }
+   @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: _createColumns(),
+      rows: _createRows(),
+    );
+  }
+}
